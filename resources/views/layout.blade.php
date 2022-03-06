4aab15ac6fb61fd45291a024d8b71aced3417ace
elayanab <jlayana@Jorge-Omen.localdomain>
2022-03-05 21:30:23 -0500

    Desing

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>Laravel en minutos</title>
+    <title>Solution Academy</title>
 
     <!-- Favicon-->
     <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
@@ -21,7 +21,9 @@
 
 </head>
 <body>
+
     <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
+        <img class="logo" src="/images/logo2.svg" alt="logo" width="10%" />
         <div class="container">
             <a class="navbar-brand" href="#page-top">Solution Academy</a>
             <button class="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
@@ -30,19 +32,32 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ms-auto">
-                    <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#portfolio">Portfolio</a></li>
+                    <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#inicio">Inicio</a></li>
+                    <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#solution">Solution</a></li>
+                    <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#oferta">Oferta Academica</a></li>
                     <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#about">About</a></li>
                     <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#contact">Contact</a></li>
                 </ul>
             </div>
         </div>
     </nav>
-    <ul>
-        <li><a href="{{ route('home') }}">Home</a></li>
-        <li><a href="{{ route('about') }}"">Acerca de...</a></li>
-        <li><a href="{{ route('blog.index') }}">Blog</a></li>
-        <li><a href="{{ route('contact') }}"">Contactos</a></li>
-    </ul>
+    <header class="masthead bg-primary text-dark text-center">
+        <div class="container d-flex align-items-center flex-column">
+            <!-- Masthead Avatar Image-->
+            <img class="masthead-avatar mb-5" src="/images/Asset3.svg" alt="..." width="500%" />
+            <!-- Masthead Heading-->
+            <h1 class="masthead-heading text-uppercase mb-0">SOLUTION ACADEMY</h1>
+            <!-- Icon Divider-->
+            <div class="divider-custom text-black">
+                <div class="divider-custom-line"></div>
+                <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                <div class="divider-custom-line"></div>
+            </div>
+            <!-- Masthead Subheading-->
+            <p class="masthead-subheading font-weight-light mb-0">Cosmetologia - Acondicionadores de Aire - Electricidad</p>
+        </div>
+    </header>
+
     <div>
         @yield('content')
     </div>
